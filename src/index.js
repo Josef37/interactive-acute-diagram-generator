e03093f410ef01d33b7cecc0849d51856e27de09
Josef Wittmann <josef.wittmann@tutanota.com>
2021-03-25 01:32:21 +0100

    Refactor main

--- src/index.js
+++ src/index.js
@@ -8,51 +8,73 @@ const board = JXG.JSXGraph.initBoard("box", {
   axis: false,
 });
 
-// Setup coordinates
-const n = 6;
-const initialVertexCoordinates = Array(n)
-  .fill()
-  .map((_value, index) => [
-    Math.cos((index * 2 * Math.PI) / n),
-    Math.sin((index * 2 * Math.PI) / n),
-    0,
-  ]);
-const initialDirectionCoordinates = initialVertexCoordinates
-  .slice(0, -1)
-  .map((vertex) => math.multiply(vertex, -0.2));
+const numberOfPoints = 6;
+const {
+  initialVertexCoordinates,
+  initialDirectionCoordinates,
+} = getInitialCoordinates(numberOfPoints);
 
-// Setup board elements
-const vertices = initialVertexCoordinates.map((vertex) =>
-  board.create("point", vertex.slice(0, 2), { withLabel: false })
-);
-const directionVertices = initialDirectionCoordinates.map((_d, i) =>
-  board.create(
-    "point",
-    math
-      .add(initialVertexCoordinates[i], initialDirectionCoordinates[i])
-      .slice(0, 2),
-    { withLabel: false }
-  )
-);
-const arrows = directionVertices.map((_v, i) => {
-  board.create("arrow", [vertices[i], directionVertices[i]]);
-});
-let edgeObjects = [];
+let { vertices, directionVertices, edgeObjects } = setupBoardElements();
 
-const buildEdges = () => {
-  const vertexCoordinates3d = vertices.map((vertex) => [
-    vertex.X(),
-    vertex.Y(),
-    0,
-  ]);
-  const directionCoordinates3d = directionVertices.map((dir, i) =>
-    math.subtract([dir.X(), dir.Y(), 0], vertexCoordinates3d[i])
+vertices
+  .concat(directionVertices)
+  .forEach((vertex) => vertex.on("drag", () => updateEdges()));
+
+updateEdges();
+
+function getInitialCoordinates(n) {
+  const initialVertexCoordinates = Array(n)
+    .fill()
+    .map((_value, index) => [
+      Math.cos((index * 2 * Math.PI) / n),
+      Math.sin((index * 2 * Math.PI) / n),
+      0,
+    ]);
+  const initialDirectionCoordinates = initialVertexCoordinates
+    .slice(0, -1)
+    .map((vertex) => math.multiply(vertex, -0.2));
+  return { initialVertexCoordinates, initialDirectionCoordinates };
+}
+
+function setupBoardElements() {
+  const vertices = initialVertexCoordinates.map((vertex) =>
+    board.create("point", vertex.slice(0, 2), { withLabel: false })
   );
+  const directionVertices = initialDirectionCoordinates.map((_d, i) =>
+    board.create(
+      "point",
+      math
+        .add(initialVertexCoordinates[i], initialDirectionCoordinates[i])
+        .slice(0, 2),
+      { withLabel: false }
+    )
+  );
+  const arrows = directionVertices.map((_v, i) => {
+    board.create("arrow", [vertices[i], directionVertices[i]]);
+  });
+  let edgeObjects = [];
+  return { vertices, directionVertices, edgeObjects };
+}
+
+function updateEdges() {
+  const { vertexCoordinates3d, directionCoordinates3d } = getCoordinates();
 
   const planes = getPlanes(vertexCoordinates3d, directionCoordinates3d);
   const edges = getEdges(vertexCoordinates3d, planes);
 
-  console.log(edgeObjects);
+  updateEdgeObjects(edges);
+}
+
+function getCoordinates() {
+  const vertexCoordinates3d = vertices.map((v) => [v.X(), v.Y(), 0]);
+  const directionCoordinates3d = directionVertices.map((d, i) =>
+    math.subtract([d.X(), d.Y(), 0], vertexCoordinates3d[i])
+  );
+  return { vertexCoordinates3d, directionCoordinates3d };
+}
+
+function updateEdgeObjects(edges) {
+  // We have to remove the points created with the edge, too.
   board.removeObject(
     edgeObjects.flatMap((edge) => [edge.point1, edge.point2, edge])
   );
@@ -64,9 +86,4 @@ const buildEdges = () => {
       fixed: true,
     })
   );
-};
-
-vertices
-  .concat(directionVertices)
-  .forEach((vertex) => vertex.on("drag", () => buildEdges()));
-buildEdges();
+}
